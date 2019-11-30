694123d2d3a57171719e125e4e7dd2b6c7447061
jacklok <jack22hk@gmail.com>
2019-11-29 21:42:53 -0800

    add about and skills

--- js/main.js
+++ js/main.js
@@ -82,38 +82,38 @@ $(document).ready(function() {
   });
 
 
-  let hint = document.getElementById('code');
-  let hint2 = document.getElementById('windowbar');
+  let code = document.getElementById('code');
+  let windowbar = document.getElementById('windowbar');
   let btn = document.getElementById('codebutton');
   let btnclose = document.getElementById('buttonclose');
 
   btn.addEventListener('click', function(){
 
       $(".header-content").css('display', 'none')
-      hint.style.display = 'block';
+      code.style.display = 'block';
       window.setTimeout(function(){
-        hint.style.opacity = 1;
-        hint.style.transform = 'scale(1)';
+        code.style.opacity = 1;
+        code.style.transform = 'scale(1)';
       },0);
 
       $(".header-content").css('display', 'none')
-      hint2.style.display = 'block';
+      windowbar.style.display = 'block';
       window.setTimeout(function(){
-        hint2.style.opacity = 1;
-        hint2.style.transform = 'scale(1)';
+        windowbar.style.opacity = 1;
+        windowbar.style.transform = 'scale(1)';
       },0);
 
   });
 
   btnclose.addEventListener('click', function(){
 
-      hint.style.opacity = 0;
-      hint2.style.opacity = 0;
-      hint.style.transform = 'scale(0)';
-      hint2.style.transform = 'scale(0)';
+      code.style.opacity = 0;
+      windowbar.style.opacity = 0;
+      code.style.transform = 'scale(0)';
+      windowbar.style.transform = 'scale(0)';
       window.setTimeout(function(){
-        hint.style.display = 'none';
-        hint2.style.display = 'none';
+        code.style.display = 'none';
+        windowbar.style.display = 'none';
       },700);
       $(".header-content").css('display', 'flex')
   
@@ -124,6 +124,111 @@ $(document).ready(function() {
   //   $("#code").css('display', 'none')
   //   $("#windowbar").css('display', 'none')
   // })
+ 
+  //  
+  var itemsMainDiv = ('.MultiCarousel');
+  var itemsDiv = ('.MultiCarousel-inner');
+  var itemWidth = "";
+
+  $('.leftLst, .rightLst').click(function () {
+      var condition = $(this).hasClass("leftLst");
+      if (condition)
+          click(0, this);
+      else
+          click(1, this)
+  });
+
+  ResCarouselSize();
+
+
+
+
+  $(window).resize(function () {
+      ResCarouselSize();
+  });
+
+  //this function define the size of the items
+  function ResCarouselSize() {
+      var incno = 0;
+      var dataItems = ("data-items");
+      var itemClass = ('.item');
+      var id = 0;
+      var btnParentSb = '';
+      var itemsSplit = '';
+      var sampwidth = $(itemsMainDiv).width();
+      var bodyWidth = $('body').width();
+      $(itemsDiv).each(function () {
+          id = id + 1;
+          var itemNumbers = $(this).find(itemClass).length;
+          btnParentSb = $(this).parent().attr(dataItems);
+          itemsSplit = btnParentSb.split(',');
+          $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+          if (bodyWidth >= 1200) {
+              incno = itemsSplit[3];
+              itemWidth = sampwidth / incno;
+          }
+          else if (bodyWidth >= 992) {
+              incno = itemsSplit[2];
+              itemWidth = sampwidth / incno;
+          }
+          else if (bodyWidth >= 768) {
+              incno = itemsSplit[1];
+              itemWidth = sampwidth / incno;
+          }
+          else {
+              incno = itemsSplit[0];
+              itemWidth = sampwidth / incno;
+          }
+          $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+          $(this).find(itemClass).each(function () {
+              $(this).outerWidth(itemWidth);
+          });
+
+          $(".leftLst").addClass("over");
+          $(".rightLst").removeClass("over");
+
+      });
+  }
+
+
+  //this function used to move the items
+  function ResCarousel(e, el, s) {
+      var leftBtn = ('.leftLst');
+      var rightBtn = ('.rightLst');
+      var translateXval = '';
+      var divStyle = $(el + ' ' + itemsDiv).css('transform');
+      var values = divStyle.match(/-?[\d\.]+/g);
+      var xds = Math.abs(values[4]);
+      if (e == 0) {
+          translateXval = parseInt(xds) - parseInt(itemWidth * s);
+          $(el + ' ' + rightBtn).removeClass("over");
+
+          if (translateXval <= itemWidth / 2) {
+              translateXval = 0;
+              $(el + ' ' + leftBtn).addClass("over");
+          }
+      }
+      else if (e == 1) {
+          var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+          translateXval = parseInt(xds) + parseInt(itemWidth * s);
+          $(el + ' ' + leftBtn).removeClass("over");
+
+          if (translateXval >= itemsCondition - itemWidth / 2) {
+              translateXval = itemsCondition;
+              $(el + ' ' + rightBtn).addClass("over");
+          }
+      }
+      $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+  }
+
+  //It is used to get some elements from btn
+  function click(ell, ee) {
+      var Parent = "#" + $(ee).parent().attr("id");
+      var slide = $(Parent).attr("data-slide");
+      ResCarousel(ell, Parent, slide);
+  }
 
 
 
