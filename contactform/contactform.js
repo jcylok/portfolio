9b166dfacb58d00df38fbf87e14be4d39d2335c6
jacklok <jack22hk@gmail.com>
2019-12-20 21:38:30 -0800

    fixed contact form

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
   "use strict";
 
   //Contact
-  $('form.contactForm').submit(function() {
+  $('.contactForm').submit(function() {
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
@@ -112,6 +112,7 @@ jQuery(document).ready(function($) {
 
       }
     });
+    alert("Thank you!")
     return false;
   });
 
